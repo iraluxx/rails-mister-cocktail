3e5d288d769d59b70df76134f597382b7b0ce132
Liyana Mahirah <nucleareactive@gmail.com>
2021-02-24 21:22:16 +0800

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Start of seed"
+
+puts "Clean DB"
+
+Ingredient.destroy_all
+
+puts "Creating 3 different Ingredients"
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "mint")
+Ingredient.create(name: "tequila")
+
+puts "Done"
